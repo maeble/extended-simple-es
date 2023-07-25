6c77f6db2d32bd0e872213c6d470b1e69120dc35
Mirabell Büscher <mirabell.buescher@web.de>
2023-07-25 13:29:40 +0200

    add results logging

--- learning_strategies/evolution/loop.py
+++ learning_strategies/evolution/loop.py
@@ -1,5 +1,6 @@
 import os
 import time
+import json
 from datetime import datetime
 from collections import deque
 
@@ -41,23 +42,41 @@ class ESLoop(BaseESLoop):
             self.coll_state_vector = False
         self.log = log
         self.save_model_period = save_model_period
+        self.run_num = datetime.now().strftime("%Y%m%d%H%M%S")
 
         # create log directory
-        now = datetime.now()
-        curr_time = now.strftime("%Y%m%d%H%M%S")
         dir_lst = []
-        dir_name = self.env.name.replace(":", "_")
-        self.save_dir = f"logs/{dir_name}/{curr_time}"
-        dir_lst.append(self.save_dir)
-        dir_lst.append(self.save_dir + "/saved_models/")
+        self.save_logs_dir = f"logs/{self.env.name}/{self.run_num}"
+        dir_lst.append(self.save_logs_dir)
+        dir_lst.append(self.save_logs_dir + "/saved_models/")
         for _dir in dir_lst:
-            print(_dir)
             os.makedirs(_dir)
+        
+        # prepare results output
+        save_results_dir = os.path.join(self.save_logs_dir, "results/")
+        save_results_filename = "results.json"
+        self.save_results_path = os.path.join(save_results_dir, save_results_filename)
+        os.makedirs(save_results_dir, exist_ok=True)
+
+        # print info
+        print("run_num:", self.run_num)
+        print("logs:", self.save_logs_dir)
 
         if self.log:
             wandb.init(project=self.env.name, config=config)
 
     def run(self):
+        # create results dict
+        results_json = {}
+        results_json["ep_length_mean"] = {}
+        results_json["ep_length_mean"]["steps"] = []
+        results_json["ep_length_mean"]["timestamps"] = []
+        results_json["ep_length_mean"]["values"] = []
+        results_json["return_mean"] = {}
+        results_json["return_mean"]["steps"] = []
+        results_json["return_mean"]["timestamps"] = []
+        results_json["return_mean"]["values"] = []
+        total_start_time = datetime.now()
 
         # init offsprings
         offsprings = self.offspring_strategy.init_offspring(
@@ -66,6 +85,7 @@ class ESLoop(BaseESLoop):
         print("Agents:", self.env.get_agent_ids(), "\n\n")
 
         prev_reward = float("-inf")
+        total_steps = 0
         ep_num = 0
         for _ in range(self.generation_num):
             start_time = time.time()
@@ -75,14 +95,14 @@ class ESLoop(BaseESLoop):
             p = mp.Pool(self.process_num)
             arguments = [(self.env, off, self.eval_ep_num, self.num_states, self.coll_state_vector) for off in offsprings]
 
-            # start ollout actors
+            # start rollout actors
             rollout_start_time = time.time()
 
             # rollout(https://stackoverflow.com/questions/41273960/python-3-does-pool-keep-the-original-order-of-data-passed-to-map)
             if self.process_num > 1:
-                results = p.map(RolloutWorker, arguments)
+                results,steps = np.swapaxes(p.map(RolloutWorker, arguments), 0,1)
             else:
-                results = [RolloutWorker(arg) for arg in arguments]
+                results,steps = np.swapaxes([RolloutWorker(arg) for arg in arguments])
             # concat output lists to single list
             p.close()
             rollout_consumed_time = time.time() - rollout_start_time
@@ -95,9 +115,27 @@ class ESLoop(BaseESLoop):
 
             # print log
             consumed_time = time.time() - start_time
+            total_time_trained = (datetime.now() - total_start_time)
+            total_time_trained = self.__strfdelta(total_time_trained, "{days} days {hours}:{minutes}:{seconds}h")
+                         
             print(
-                f"episode: {ep_num}, Best reward: {best_reward:.2f}, sigma: {curr_sigma:.3f}, time: {consumed_time:.2f}, rollout_t: {rollout_consumed_time:.2f}, eval_t: {eval_consumed_time:.2f}"
+                f"episode: {ep_num}, Best reward: {best_reward:.2f}, sigma: {curr_sigma:.3f}, time: {consumed_time:.2f}, rollout_t: {rollout_consumed_time:.2f}, eval_t: {eval_consumed_time:.2f}, total_time_trained: {total_time_trained}"
             )
+
+            # save results to json file   
+            ep_length = steps[0]          
+            total_steps = ep_length+total_steps
+            print(total_steps)
+            if (ep_num-1) % self.save_model_period == 0: 
+                timestamp = datetime.now().strftime("%Y-%m-%dT0%H:%M:%S.%s")    
+                results_json["return_mean"]["steps"].append(total_steps)
+                results_json["return_mean"]["timestamps"].append(timestamp)
+                results_json["return_mean"]["values"].append(np.mean(results)) # TODO or best reward? or sum?
+                results_json["ep_length_mean"]["steps"].append(total_steps)
+                results_json["ep_length_mean"]["timestamps"].append(timestamp)
+                results_json["ep_length_mean"]["values"].append(ep_length)
+                with open (self.save_results_path, "w") as outfile:
+                    outfile.write(json.dumps(results_json, indent=4))
 
             prev_reward = best_reward
             if self.log:
@@ -108,20 +146,32 @@ class ESLoop(BaseESLoop):
                 )
 
             elite = self.offspring_strategy.get_elite_model()
-            if ep_num % self.save_model_period == 0:
-                save_pth = self.save_dir + "/saved_models" + f"/ep_{ep_num}.pt"
+            if (ep_num-1) % self.save_model_period == 0:
+                save_pth = self.save_logs_dir + "/saved_models" + f"/ep_{ep_num}.pt"
                 torch.save(elite.state_dict(), save_pth)
+
+        # finally
+
+    def __strfdelta(self, tdelta, fmt):
+        d = {"days": tdelta.days}
+        d["hours"], rem = divmod(tdelta.seconds, 3600)
+        d["minutes"], d["seconds"] = divmod(rem, 60)
+        return fmt.format(**d)
+            
 
 
 # offspring_id, worker_id, eval_ep_num=10
-def RolloutWorker(arguments, debug=False):
+def RolloutWorker(arguments, debug=False): # TODO shared state option
     env, offspring, eval_ep_num, num_states, coll_state_vector = arguments
     total_reward = 0
+    
     for _ in range(eval_ep_num):
         states = env.reset()
         done = False
         for k, model in offspring.items():
             model.reset()
+
+        steps = 0
         while not done:
             actions = {}
             s = np.array(states[k]["state"])[np.newaxis, ...]
@@ -140,13 +190,18 @@ def RolloutWorker(arguments, debug=False):
                     model_s = __RolloutWorkerModelIterate(model, s, num_states, debug)
                     actions[k] = model_s
             states, r, done, _ = env.step(actions)
+            #print("states=",states)
+            steps += 1
+
             #env.render()
             if coll_state_vector:
                 total_reward += sum(r)
             else:
                 total_reward += r
+
+        #print("steps:", steps)
     rewards = total_reward / eval_ep_num
-    return rewards
+    return [rewards,steps]
 
 def __RolloutWorkerModelIterate(model, s, num_states, debug):
     if debug:
