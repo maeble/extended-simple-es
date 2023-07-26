1a265613273ae6b06d7a437ef3cb14e4b9c14a04
Mirabell Büscher <mirabell.buescher@web.de>
2023-07-26 12:36:19 +0200

    make neural network similar to epymarl network

--- networks/neural_network.py
+++ networks/neural_network.py
@@ -10,46 +10,49 @@ class GymEnvModel(BaseNetwork):
     def __init__(self, num_state=8, num_action=4, discrete_action=True, gru=True):
         super(GymEnvModel, self).__init__()
         self.num_action = num_action
-        self.fc1 = nn.Linear(num_state, 32) # Applies a linear transformation to the incoming data
         self.use_gru = gru
-        if self.use_gru:
-            self.gru = nn.GRU(32, 32)
-            self.h = torch.zeros([1, 1, 32], dtype=torch.float)
-        self.fc2 = nn.Linear(32, num_action)
         self.discrete_action = discrete_action
+        self.hidden_dimension = 64
+        # nn model --------------------------------------------------------------------------
+        # similar to EpyMARL RNNAgent Network Model
+        self.fc1 = nn.Linear(num_state, self.hidden_dimension) # Applies a linear transformation to the incoming data
+        self.h = self.init_hidden() # hidden state
+        if self.use_gru: # else: fc
+            self.gru = nn.GRU(self.hidden_dimension, self.hidden_dimension) # multi-layer gated recurrent unit 
+        else:
+            self.fc2 = nn.Linear(self.hidden_dimension, self.hidden_dimension) 
+        self.fc3 = nn.Linear(self.hidden_dimension, num_action)
+        # -----------------------------------------------------------------------------------     
+
+    def init_hidden(self):
+        return torch.zeros([1, 1, 64], dtype=torch.float) 
 
     def forward(self, x, debug=False):
         with torch.no_grad():
-            if debug:
-                print("1: ", x, np.array(x).shape)
             x = torch.from_numpy(x).float()
             x = x.unsqueeze(0)
-            if debug:
-                print("2: ", x, np.array(x).shape)
-            x_ = self.fc1(x)
-            if debug:
-                print("3: ", x_, np.array(x_).shape)
-
-            x = torch.tanh(x_)
+            
+            x = self.fc1(x) # in
+            x = F.relu(x) 
             if self.use_gru:
-                x, self.h = self.gru(x, self.h)
-                x = torch.tanh(x)
-            x = self.fc2(x)
-            if debug:
-                print("4:", x)
+                x, self.h = self.gru(x, self.h) # multi-layer
+            else:
+                x = self.fc2(x)
+                x = F.relu(x) 
+            x = self.fc3(x) # out
+
             if self.discrete_action:
                 x = F.softmax(x.squeeze(), dim=0)
                 x = torch.argmax(x)
             else:
                 x = torch.tanh(x.squeeze())
             x = x.detach().cpu().numpy()
-            if debug:
-                print(x)
+
             return x
 
     def reset(self):
         if self.use_gru:
-            self.h = torch.zeros([1, 1, 32], dtype=torch.float)
+            self.h = self.init_hidden()
 
     def zero_init(self):
         for param in self.parameters():
